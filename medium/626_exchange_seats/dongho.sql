b37f45530f14813b831a5b6fb120bc97deaf2cad
donghokim-DBA <dongho527@naver.com>
2019-11-12 21:12:04 +0900

    create 626_exchange_seats
    create 626_exchange_seats

---
+++ medium/626_exchange_seats/dongho.sql
@@ -0,0 +1,12 @@
+--https://leetcode.com/problems/exchange-seats/submissions/
+--Runtime: 565 ms, faster than 83.87% of MS SQL Server online submissions for Exchange Seats.
+--Memory Usage: 0B, less than 100.00% of MS SQL Server online submissions for Exchange Seats.
+
+WITH new_seat(newid, student) 
+AS (SELECT (CASE WHEN id%2 = 0 THEN id-1 
+                 ELSE id+1 END) AS newid, student
+      FROM seat)
+
+SELECT RANK()OVER(ORDER BY newid) AS id, 
+       student
+  FROM new_seat
