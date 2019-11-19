5917a4cda280dd74a6e2cfc7f4835c743451cd02
donghokim-DBA <dongho527@naver.com>
2019-11-19 22:37:48 +0900

    [beta] Biggest in Each Column

---
+++ codewars/beta/Biggest in Each Column/dongho.sql
@@ -0,0 +1,8 @@
+--https://www.codewars.com/kata/5946e1947325ad198e0000a7/train/sql
+
+SELECT MAX(number1) as MAXNUM1,
+       MAX(number2) as maxnum2,
+       MAX(number3) as maxnum3,
+       MAX(number4) as maxnum4,
+       MAX(number5) as maxnum5
+  FROM numbers
